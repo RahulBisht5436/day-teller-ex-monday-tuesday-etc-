4159047383c614f8fc12a0f14e0a4f78f8a050e9
Rahul bisht <Rahulbisht5436@gmail.com>
2021-06-14 08:24:05 +0530

    Update DayTeller.py

--- DayTeller.py
+++ DayTeller.py
@@ -1,47 +1,114 @@
-def day(y,x):
+
+def day(d,m,y):
+  if m==1:
+    u=d
+
+
+  if m==2:
+    u=31+d
+    print("working")
+
+
+  if m==3:
+    if y%4==0:
+      u=31+29+d
+    else:
+      u=31+28+d
+
+
+  if m==4:
+    if y%4==0:
+      u=31+29+31+d
+    else:
+      u=31+28+31+d
+
+
+
+  if m==5:
+    if y%4==0:
+      u=31+29+31+30+d
+    else:
+      u=31+28+31+30+d
+
   
-  if x%7==y:
-    print("wednesday")
-  elif x%7==y+1:
-    print("thusday")
-  elif x%7==y+2:
-    print("friday")
-  elif x%7==y+3:
-    print("saturday")
-  elif x%7==y+4:
-    print("sunday")
-  elif x%7==y+5:
+  if m==6:
+    if y%4==0:
+      u=31+31+31+30+29+d
+    else:
+      u=31+28+31+31+30+d
+
+  
+  if m==7:
+    if y%4==0:
+      u=31+31+31+30+30+29+d
+    else:
+      u=31+28+d+31+31+30+30
+
+  
+  if m==8:
+    if y%4==0:
+      u=31+31+31+31+30+30+29+d
+    else:
+      u=31+28+d+31+31+31+30+30+30
+
+  
+  if m==9:
+    if y%4==0:
+      u=31+29+d+31+31+31+31+30+30+30
+    else:
+      u=31+28+d+31+31+31+31+30+30+30+30
+
+  
+  if m==10:
+    if y%4==0:
+      u=31+29+d+31+31+31+31+30+30+30+30+31
+    else:
+      u=31+28+d+31+31+31+31+30+30+30+30+31
+
+  
+  if m==11:
+    if y%4==0:
+      u=31+29+d+31+31+31+31+30+30+30+30+31+30
+    else:
+      u=31+28+d++31+31+31+31+30+30+30+30+31+30
+
+  
+  if m==12:
+    if y%4==0:
+      u=31+29+d+31+31+31+31+31+30+30+30+30+31+30
+    else:
+      u=31+28+d+31+31+31+31+30+30+30+30+31+30+31
+
+  return u    
+
+def day1(date_):
+  z=4
+  if (date_+z)%7==1:
     print("monady")
-  else:
-    print("tuesday")      
+  elif (date_+z)%7==2:
+    print("tuesday")  
+  elif (date_+z)%7==3:
+    print("wednesday")  
+  elif (date_+z)%7==4:
+    print("thusday")  
+  elif (date_+z)%7==5:
+    print("friday")  
+  elif (date_+z)%7==6:
+    print("saturday")  
+  elif date_%7==0:
+    print("sunday")  
+    
+d=int(input("enter date"))
+m=int(input("enter month"))
+y=int(input("enter year"))
+ply=int(((y-1)-20)/4)
+date_=day(d,m,y)
+date_=date_+ply+(y-21)*365
+day1(date_)
 
-def month(z,x):
-  y=z
-  if y ==1:
-    day(6,x)
-  elif  y==2:
-    day(3,x)
-  elif  y==3:
-    day(3,x)
-  elif  y==4:
-    day(0,x)
-  elif  y==5:
-    day(5,x)
-  elif  y==6:
-    day(2,x)
-  elif  y==7:
-    day(0,x)
-  elif  y==8:
-    day(4,x)
-  elif  y==9:
-    day(1,x)
-  elif  y==10:
-    day(6,x)
-  elif  y==11:
-    day(3,x)
-  else:
-    day(1,x)      
 
-z=int(input("whats is month" ))
-x=int(input("whats is  date" ))
-month(z,x)                
+  
+  
+        
+
+
